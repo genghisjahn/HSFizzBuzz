918989038c0c29f53361da902fb2d7170adbb189
Jon M. Wear <jon.m.wear@gmail.com>
2013-09-12 01:44:33 -0400

    Combined test complete
    refactor in the morning.

--- FizzBuzzTests/MainTests.cs
+++ FizzBuzzTests/MainTests.cs
@@ -26,7 +26,7 @@ namespace FizzBuzzTests
         {
             var numtocheck = 1001;
             var fbpitem = new FBPalindromeItem(1);
-            var result= fbpitem.GetResult(numtocheck);
+            var result = fbpitem.GetResult(numtocheck);
             Assert.IsTrue(result.Text == "Palindrome!" && result.FBValue == numtocheck);
         }
 
@@ -148,41 +148,6 @@ namespace FizzBuzzTests
         public void All_3_Fizz_5_Buzz_15_FizzBuzz_Test()
         {
             var m15 = GetMultiplesByDivisor(15, numbers);
-            var m3 = GetMultiplesByDivisor(3, numbers).Where(n=>!m15.Contains(n)).ToArray();
-            var m5 = GetMultiplesByDivisor(5, numbers).Where(n => !m15.Contains(n)).ToArray();
-            
-
-            var other = numbers.Where(n => !m3.Contains(n) && !m5.Contains(n) && !m15.Contains(n)).ToArray();
-
-            var fb15 = new FBDivisorItem(15, "FizzBuzz");
-            var fb5 = new FBDivisorItem(5, "Buzz");
-            var fb3 = new FBDivisorItem(3, "Fizz");
-            
-            
-
-            var fbproc = new FBProcessor(numbers,new List<IFBItem> { fb15, fb5, fb3 });
-
-            var procresults = fbproc.GetResults();
-            
-            var m3count = procresults.Where(n => n.Text == "Fizz").Count();
-            var m5count = procresults.Where(n => n.Text == "Buzz").Count();
-            var m15count = procresults.Where(n => n.Text == "FizzBuzz").Count();
-            var othercount = procresults.Where(n => n.Text == null).Count();
-            
-            Assert.IsTrue(m3.Count() == m3count && m5.Count() == m5count && m15.Count() == m15count && othercount == other.Count());
-
-        }
-
-
-        #endregion
-
-        #region "Test Other And Fizz Buzz"
-
-        [TestMethod]
-        [TestCategory("FizzBuzz & Other Tests")]
-        public void All_Palindrome_PerfectSqaure_FizzBuzz_Test()
-        {
-            var m15 = GetMultiplesByDivisor(15, numbers);
             var m3 = GetMultiplesByDivisor(3, numbers).Where(n => !m15.Contains(n)).ToArray();
             var m5 = GetMultiplesByDivisor(5, numbers).Where(n => !m15.Contains(n)).ToArray();
 
@@ -208,6 +173,59 @@ namespace FizzBuzzTests
 
         }
 
+
+        #endregion
+
+        #region "Test Other And Fizz Buzz"
+
+        [TestMethod]
+        [TestCategory("FizzBuzz & Other Tests")]
+        public void All_Palindrome_PerfectSqaure_FizzBuzz_Test()
+        {
+            var sq = new int[] { 1, 4, 9, 16 };
+
+            //Single digits are counted as pailndromes, but the perfect squares are left out.
+            //So 1,4 & 9 are not included.
+            var p = new int[] {  2, 3,  5, 6, 7, 8,  11 }; 
+            
+
+            var m15 = GetMultiplesByDivisor(15, numbers20).Where(n => !p.Contains(n) && !sq.Contains(n)).ToArray();
+            var m3 = GetMultiplesByDivisor(3, numbers20).Where(n => !m15.Contains(n) && !p.Contains(n) && !sq.Contains(n)).ToArray();
+            var m5 = GetMultiplesByDivisor(5, numbers20).Where(n => !m15.Contains(n) && !p.Contains(n) && !sq.Contains(n)).ToArray();
+
+
+            var other = numbers20.Where(n => !m3.Contains(n) && !m5.Contains(n) && !m15.Contains(n) && !p.Contains(n) && !sq.Contains(n)).ToArray();
+
+            var fb15 = new FBDivisorItem(15, "FizzBuzz");
+            var fb5 = new FBDivisorItem(5, "Buzz");
+            var fb3 = new FBDivisorItem(3, "Fizz");
+            var fbp = new FBPalindromeItem(20);
+            var fbsq = new FBPerfectSquareItem(25);
+
+
+            var fbproc = new FBProcessor(numbers20, new List<IFBItem> { fb15, fb5, fb3, fbp, fbsq });
+
+            var procresults = fbproc.GetResults();
+
+            var m3count = procresults.Where(n => n.Text == "Fizz").Count();
+            var m5count = procresults.Where(n => n.Text == "Buzz").Count();
+            var m15count = procresults.Where(n => n.Text == "FizzBuzz").Count();
+            var pcount = procresults.Where(n => n.Text == "Palindrome!").Count();
+            var sqcount = procresults.Where(n => n.Text == "Perfect Square!").Count();
+
+            var othercount = procresults.Where(n => n.Text == null).Count();
+
+            Assert.IsTrue(
+                    m3count == m3.Count()
+                    && m5count == m5.Count()
+                    && m15count == m15.Count()
+                    && othercount == other.Count()
+                    && pcount == p.Count()
+                    && sqcount == sq.Count()
+                    );
+
+        }
+
         #endregion
 
         #region "Private Methods"
@@ -219,7 +237,7 @@ namespace FizzBuzzTests
             bool conditioncheck = false;
             foreach (int i in numstocheck)
             {
-                foreach (IFBItem fbi in fbitems.OrderByDescending(fb=>fb.OrderCheck))
+                foreach (IFBItem fbi in fbitems.OrderByDescending(fb => fb.OrderCheck))
                 {
                     FBResult fbresult = fbi.GetResult(i);
                     if (fbresult.Text == textcheck) break;
@@ -233,9 +251,9 @@ namespace FizzBuzzTests
             return conditioncheck;
         }
 
-        private static int[] GetMultiplesByDivisor(int divisor,int[] nums)
+        private static int[] GetMultiplesByDivisor(int divisor, int[] nums)
         {
-            int[] result = nums.Where(n => n % divisor == 0 ).ToArray();
+            int[] result = nums.Where(n => n % divisor == 0).ToArray();
             return result;
         }
         private static int[] GetNonMultiplesByDivisor(int divisor, int[] nums)
