e61d434585b644e715e1394a3be7bd53993a42cf
Jon M. Wear <jon.m.wear@gmail.com>
2013-09-11 23:59:19 -0400

    Changed variable names and one method name.

--- FizzBuzzTests/MainTests.cs
+++ FizzBuzzTests/MainTests.cs
@@ -23,35 +23,56 @@ namespace FizzBuzzTests
         {
             var numstocheck = GetMultiplesByDivisor(3, numbers);
             var fbitem3 = new FBDivisorItem(3, "Fizz");
-            bool nofizzcheck = CheckAllForSingleCondition(numstocheck,new List<IFBItem> { fbitem3 },"Fizz");    
-            Assert.IsTrue(nofizzcheck == false);
+            bool condcheck = CheckAllForSingleCondition(numstocheck, new List<IFBItem> { fbitem3 }, "Fizz");
+            Assert.IsTrue(condcheck == false);
         }
+
         [TestMethod]
         public void NonMultiples_Of_3_Return_Number_Test()
         {
             var notby3 = GetNonMultiplesByDivisor(3, numbers);
             var fbitem3 = new FBDivisorItem(3, "Fizz");
-            bool fizzcheck = CheckAllForSingleCondition(notby3, new List<IFBItem> { fbitem3 }, null);
+            bool condcheck = CheckAllForSingleCondition(notby3, new List<IFBItem> { fbitem3 }, null);
 
-            Assert.IsTrue(fizzcheck == false);
+            Assert.IsTrue(condcheck == false);
 
         }
+
         [TestMethod]
         public void Multiples_Of_5_Return_Buzz_Test()
         {
             var numstocheck = GetMultiplesByDivisor(5, numbers);
             var fbitem5 = new FBDivisorItem(5, "Buzz");
-            bool nofizzcheck = CheckAllForSingleCondition(numstocheck, new List<IFBItem> { fbitem5 }, "Buzz");
-            Assert.IsTrue(nofizzcheck == false);
+            bool condcheck = CheckAllForSingleCondition(numstocheck, new List<IFBItem> { fbitem5 }, "Buzz");
+            Assert.IsTrue(condcheck == false);
         }
         [TestMethod]
         public void NonMultiples_Of_5_Return_Number_Test()
         {
             var notby5 = GetNonMultiplesByDivisor(5, numbers);
             var fbitem5 = new FBDivisorItem(5, "Buzz");
-            bool fizzcheck = CheckAllForSingleCondition(notby5, new List<IFBItem> { fbitem5 }, null);
+            bool condcheck = CheckAllForSingleCondition(notby5, new List<IFBItem> { fbitem5 }, null);
 
-            Assert.IsTrue(fizzcheck == false);
+            Assert.IsTrue(condcheck == false);
+
+        }
+
+        [TestMethod]
+        public void Multiples_Of_15_Return_FizzBuzz_Test()
+        {
+            var numstocheck = GetMultiplesByDivisor(15, numbers);
+            var fbitem15 = new FBDivisorItem(15, "FizzBuzz");
+            bool condcheck = CheckAllForSingleCondition(numstocheck, new List<IFBItem> { fbitem15 }, "FizzBuzz");
+            Assert.IsTrue(condcheck == false);
+        }
+        [TestMethod]
+        public void NonMultiples_Of_15_Return_Number_Test()
+        {
+            var notby15 = GetNonMultiplesByDivisor(15, numbers);
+            var fbitem15 = new FBDivisorItem(15, "FizzBuzz");
+            bool condcheck = CheckAllForSingleCondition(notby15, new List<IFBItem> { fbitem15 }, null);
+
+            Assert.IsTrue(condcheck == false);
 
         }
        
