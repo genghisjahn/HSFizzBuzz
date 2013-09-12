d39f421cf2e51b65bc7959ecca1add42626e5240
Jon M. Wear <jon.m.wear@gmail.com>
2013-09-11 21:16:42 -0400

    Initial Commit

---
+++ FizzBuzzTests/MainTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class MainTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
